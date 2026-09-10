7ccc25a69c18ad81222d032e14791d424a831a23
Joe Rhodes <code@joelrhodes.dev>
2026-09-10 16:22:22 -0500

    Add Server definition
    - Create a table and model to hold a server definition
    - Create a Enum to represent server connection status

---
+++ app/Models/Server.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ServerStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Server extends Model
+{
+    /** @use HasFactory<\Database\Factories\ServerFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'hostname',
+        'ip_address',
+        'ssh_port',
+        'ssh_user',
+        'status',
+        'last_checked_at',
+        'last_check_output',
+        'notes',
+    ];
+
+    protected $casts = [
+        'status' => ServerStatus::class,
+        'last_checked_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2026_09_10_211519_create_servers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('hostname')->nullable();
+            $table->string('ip_address');
+            $table->unsignedSmallInteger('ssh_port')->default(22);
+            $table->string('ssh_user');
+            $table->string('status')->default('pending');
+            $table->timestamp('last_checked_at')->nullable();
+            $table->text('last_check_output')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servers');
+    }
+};
