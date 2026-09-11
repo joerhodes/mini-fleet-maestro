0d0eacdab64087e033eeb572713eaae7a47acb6c
Joe Rhodes <joe.rhodes@fusemind.com>
2026-09-11 16:48:30 -0500

    Remove dead route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    //Route::view('dashboard', 'dashboard')->name('dashboard');
     Route::get('dashboard', \App\Http\Controllers\DashBoardController::class)->name('dashboard');
 });
 
