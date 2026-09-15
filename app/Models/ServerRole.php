e321899d6a71022487377f331aa54ac23319b3bc
Joe Rhodes <joe.rhodes@fusemind.com>
2026-09-15 06:23:45 -0500

    Add Roles and Config
    - map server and roles

--- app/Models/ServerRole.php
+++ app/Models/ServerRole.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ServerRole extends Model
 {
     /** @use HasFactory<\Database\Factories\ServerRoleFactory> */
     use HasFactory;
+
+    protected $fillable = ['server_id', 'role'];
+
+    public function server(): BelongsTo
+    {
+        return $this->belongsTo(Server::class);
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
